f780904e5b401c726b22e27cdd5447637c0706e2
Igorth <igordiasth@gmail.com>
2020-02-21 14:32:36 +1300

    class

--- CursoES6/main.js
+++ CursoES6/main.js
@@ -1,9 +1,41 @@
-class Teste{
-    metodo(){
-
+class List{
+    constructor(){
+        this.data = []
     }
 
-    outro(){
-
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List{
+    constructor(){
+        super();
+
+        this.usuario = 'Igor';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novoTodo').onclick = function(){
+    MinhaLista.add('Novo Tudo');
+}
+
+MinhaLista.mostraUsuario();
+
+
+
+
+class Matematica{
+    static soma(a, b){
+        return a + b;
+    }
+}
+
+console.log(Matematica.soma(1, 2));
