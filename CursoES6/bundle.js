96840266aa887d947f19d2c438a86822f4b48b83
Igorth <igordiasth@gmail.com>
2020-02-22 18:51:55 +1300

    challenge 2

--- CursoES6/bundle.js
+++ CursoES6/bundle.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
@@ -69,3 +75,37 @@ console.log(Adm1.isAdmin()); // true
 
 var adm2 = new Admin('teste@tetste.com', '123123');
 console.log(adm2.isAdmin()); // 2 EXERCICIO
+
+var usuarios = [{
+  nome: "Igor",
+  idade: 23,
+  empresa: "Rocketseat"
+}, {
+  nome: "laisa",
+  idade: 15,
+  empresa: "Rocketseat"
+}, {
+  nome: "lfds",
+  idade: 30,
+  empresa: "bora"
+}];
+var idades = usuarios.map(function (item) {
+  return item.idade;
+});
+console.log(idades);
+var filter = usuarios.filter(function (item) {
+  return item.idade >= 18 && item.empresa === "Rocketseat";
+});
+console.log(filter);
+var find = usuarios.find(function (item) {
+  return item.empresa === "Google";
+});
+console.log(find);
+var mult = usuarios.map(function (item) {
+  return _objectSpread({}, item, {
+    idade: item.idade * 2
+  });
+}).filter(function (item) {
+  return item.idade <= 50;
+});
+console.log(mult);
--- CursoES6/desafios.js
+++ CursoES6/desafios.js
@@ -37,3 +37,33 @@ console.log(adm2.isAdmin());
 
 // 2 EXERCICIO
 
+const usuarios = [
+    { nome: "Igor", idade: 23, empresa: "Rocketseat"},
+    { nome: "laisa", idade: 15, empresa: "Rocketseat"},
+    { nome: "lfds", idade: 30, empresa: "bora"},
+];
+
+const idades = usuarios.map(function(item){
+    return item.idade;
+})
+
+console.log(idades);
+
+
+const filter = usuarios.filter(function(item){
+    return item.idade >=18 && item.empresa === "Rocketseat"
+})
+
+console.log(filter)
+
+
+const find = usuarios.find(function(item){
+    return item.empresa === "Google";
+})
+console.log(find);
+
+
+const mult = usuarios.map(item => ({...item, idade: item.idade * 2}))
+    .filter(item => item.idade <= 50)
+
+console.log(mult)
