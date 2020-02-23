49a64d953e0285ef909b8bec8f639843d27127da
Igorth <igordiasth@gmail.com>
2020-02-24 09:46:02 +1300

    challenge 5

--- CursoES6/desafios.js
+++ CursoES6/desafios.js
@@ -157,3 +157,31 @@ function mostraInfos({nome, endereco:{cidade, estado}}){
 }
 
 console.log(mostraInfos(empresa));
+
+
+//5.1 REST
+
+const arr = [1, 2, 3, 4, 5, 6]
+const [x, ...y] = arr;
+
+console.log(x); // 1
+console.log(y); // [2, 3, 4, 5, 6]
+
+
+//5.2 SPREAD
+
+const usuario = {
+    nome: 'Diego',
+    idade: 23,
+    endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil',
+    }
+};
+
+const usuario2 = {...usuario, nome:"Gabriel"};
+const usuario3 = {...usuario, endereco:{cidade:"Lontras"}};
+
+console.log(usuario2);
+console.log(usuario3);
